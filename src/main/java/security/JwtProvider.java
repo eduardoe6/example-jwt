ec2dfaa2db32b1a8d10c9fdb71c965f7f2cda24a
Eduardo Chaves da Silva <edueduardo0699@gmail.com>
2019-11-07 20:31:09 -0300

    init

--- src/main/java/security/JwtProvider.java
+++ src/main/java/security/JwtProvider.java
@@ -1,13 +1,29 @@
 package security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class JwtProvider {
 
-    public String generateToken(Authentication auth){
+    private static final String TOKEN_KEY = "%$#--abc666-66**#%%@";
+
+    public String generateToken(Authentication auth) {
         var userDetails = (UserAuthDetails) auth.getPrincipal();
+
+        var creationTime = new Date();
+        var expirationTime = new Date(creationTime.getTime() + (1000 * 60 * 60 * 24 * 100));
+
+         return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(creationTime)
+                .setExpiration(expirationTime)
+                .signWith(SignatureAlgorithm.HS512, TOKEN_KEY)
+                .compact();
     }
 
 }
